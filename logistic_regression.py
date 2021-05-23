13b0b6f378d523a706d1ac7dc7bf5161cca44132
Chinedu Ezeofor <Chineiduezeofor@gmail.com>
2021-05-23 21:04:48 +0100

    modified logistic_regression.py

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,35 +1,47 @@
 import numpy as np
 
 class LogisticRegression:
-	def __init__(self, learning_rate: int=0.0001, n_iters: int=10_000):
-		self.learning_rate = learning_rate
-		self.n_iters = n_iters
-		self.weights = 0
-		self.bias = 0
+    def __init__(self, learning_rate: int=0.0001, n_iters: int=10_000):
+        self.learning_rate = learning_rate
+        self.n_iters = n_iters
+        self.weights = 0
+        self.bias = 0
 
 
-	def fit(self, X, y):
-		# train the model
-		n_samples, n_features = X.shape
-		self.weights = np.zeros(n_features)
+    def fit(self, X, y):
+        # train the model
+        n_samples, n_features = X.shape
+        self.weights = np.zeros(n_features)
 
-		# gradient descent
-		for _ in np.arange(self.n_iters):
-			# linear model
-			y_linear = self.bias + np.dot(X, self.weights)
-			
-			# approximate y_linear to have values between 0 and 1
-			y_pred = 1 / (1 + np.exp(y_linear))   # sigmoid function
+        # gradient descent
+        for _ in np.arange(self.n_iters):
+            # linear model
+            y_linear = self.bias + np.dot(X, self.weights)   
+            # approximate y_linear to have values between 0 and 1
+            # y_pred = 1 / (1 + np.exp(-y_linear))   # sigmoid function
+            y_pred = self.sigmoid_func(y_linear)
 
-			d_weights = (1 / n_samples) * (2 * np.dot(X.T, (y_pred - y)))
-			d_bias = (1 / n_samples) * (2 * np.sum(y_pred, y))
+            d_weights = (1 / n_samples) * (2 * np.dot(X.T, (y_pred - y)))
+            d_bias = (1 / n_samples) * (2 * np.sum(y_pred - y))
 
-			# update the values
-			self.weights -= self.learning_rate * d_weights
-			self.bias-= self.learning_rate * d_bias
+            # update the values
+            self.weights -= self.learning_rate * d_weights
+            self.bias -= self.learning_rate * d_bias
 
-		def predict(self, X):
-			"""Make classifications."""
-			y_linear = self.bias + np.dot(X, self.weights)
-			y_pred = y_pred = 1 / (1 + np.exp(y_linear))
-			clf = [1 if val > 0.5 else 0 for val in y_pred]
+    def predict(self, X):
+        """Make classifications."""
+        y_linear = self.bias + np.dot(X, self.weights)
+        # y_pred = y_pred = 1 / (1 + np.exp(-y_linear))
+        y_pred = self.sigmoid_func(y_linear)
+        clf = [1 if val > 0.5 else 0 for val in y_pred]
+        # convert to an array
+        clf = np.array(clf)
+        return clf
+
+    def sigmoid_func(self, val: int) -> int:
+      """Returns values between 0 and 1 inclusive."""
+      result = 1 / (1 + np.exp(-val))
+      return result
+
+
+
