bbc5cca3cac6e87d2937402d5cf37d7925341fea
Chinedu Ezeofor <Chineiduezeofor@gmail.com>
2021-05-24 06:33:47 +0100

    refactored linear and logistic.py

--- linear_regression.py
+++ linear_regression.py
@@ -3,30 +3,33 @@ import pandas as pd
 
 #### Linear Regression (from scratch)
 class LinearRegression:
-	def __init__(self, learning_rate: int=0.0001, n_iters: int=10_000):
-		"""Initialize the parameters"""
-		self.learning_rate = learning_rate
-		self.n_iters = n_iters
-		self.wieghts = 0
-		self.bias = 0
-
-	def fit(self, X, y):
-		"""Train the model."""
-		n_samples, n_features = X.shape
-		self.weights = np.zeros(n_features)  # initialize the weights with zeros
-		
-		for i in np.arange(self.n_iters):
-			# make predictions
-			y_pred = self.bias + np.dot(X, self.weights)
-
-			# gradient descent
-			d_weights = (1 / n_samples) * (2 * np.dot(X.T, (y_pred - y)))
-			d_bias = (1 / n_samples) * (2 * np.sum(y_pred - y))
-
-			# update the parameters
-			self.weights -= self.learning_rate * d_weights
-			self.bias -= self.learning_rate * d_bias
-
-	def predict(self, X):
-		y_predicted = self.bias + np.dot(X, self.weights)
-		return y_predicted
+    def __init__(self, n_iters: int=10_000, learning_rate: int=0.0001):
+        self.n_iters = n_iters
+        self.l_r = learning_rate
+        self.weights = 0
+        self.bias = 0
+        
+    def fit(self, X, y):
+        # set the parameters
+        n_samples, n_features = X.shape
+        # assign the weights using the n_features
+        self.weights = np.zeros(n_features)
+        
+        # gradient descent
+        for _ in np.arange(self.n_iters):
+            y_pred = self.bias + np.dot(X, self.weights)
+            
+            # comute the change in values
+            dw = (2 * np.dot(X.T, (y_pred - y))) / n_samples
+            db = (2 * np.sum(y_pred - y)) / n_samples
+            # update the values
+            self.weights -= self.l_r * dw 
+            self.bias -= self.l_r * db 
+            
+    def predict(self, X):
+        y_pred = self.bias + np.dot(X, self.weights)
+        return y_pred
+    
+    def _mse(self, y_true: np.array, y_pred: np.array) -> float:
+        mse = np.mean(np.square(y_true - y_pred))
+        return mse
--- logistic_regression.py
+++ logistic_regression.py
@@ -1,47 +1,44 @@
 import numpy as np
 
+#### Logistic Regression (from scratch)
 class LogisticRegression:
-    def __init__(self, learning_rate: int=0.0001, n_iters: int=10_000):
-        self.learning_rate = learning_rate
+    def __init__(self, n_iters: int=10_000, learning_rate: int=0.0001):
         self.n_iters = n_iters
+        self.l_r = learning_rate
         self.weights = 0
         self.bias = 0
-
-
+        
     def fit(self, X, y):
-        # train the model
+        # set the parameters
         n_samples, n_features = X.shape
+        # assign the weights using the n_features
         self.weights = np.zeros(n_features)
-
+        
         # gradient descent
         for _ in np.arange(self.n_iters):
-            # linear model
-            y_linear = self.bias + np.dot(X, self.weights)   
-            # approximate y_linear to have values between 0 and 1
-            # y_pred = 1 / (1 + np.exp(-y_linear))   # sigmoid function
-            y_pred = self.sigmoid_func(y_linear)
-
-            d_weights = (1 / n_samples) * (2 * np.dot(X.T, (y_pred - y)))
-            d_bias = (1 / n_samples) * (2 * np.sum(y_pred - y))
-
+            y_lin = self.bias + np.dot(X, self.weights)
+            y_pred = self._sigmoid_func(y_lin)
+            
+            # comute the change in values
+            dw = (2 * np.dot(X.T, (y_pred - y))) / n_samples
+            db = (2 * np.sum(y_pred - y)) / n_samples
             # update the values
-            self.weights -= self.learning_rate * d_weights
-            self.bias -= self.learning_rate * d_bias
-
+            self.weights -= self.l_r * dw 
+            self.bias -= self.l_r * db 
+            
     def predict(self, X):
-        """Make classifications."""
-        y_linear = self.bias + np.dot(X, self.weights)
-        # y_pred = y_pred = 1 / (1 + np.exp(-y_linear))
-        y_pred = self.sigmoid_func(y_linear)
-        clf = [1 if val > 0.5 else 0 for val in y_pred]
-        # convert to an array
-        clf = np.array(clf)
-        return clf
-
-    def sigmoid_func(self, val: int) -> int:
-      """Returns values between 0 and 1 inclusive."""
-      result = 1 / (1 + np.exp(-val))
-      return result
+        y_lin = self.bias + np.dot(X, self.weights)
+        y_pred = self._sigmoid_func(y_lin)
+        # make classifications
+        clf = [1 if i > 0.50 else 0 for i in y_pred]
+        return np.array(clf)
+    
+    def _sigmoid_func(self, val: int) -> int:
+        return 1 / (1 + np.exp(-val))
+    
+    def _accuracy(self,  y_true: np.array, y_pred: np.array) -> float:
+        acc = np.sum(y_true == y_pred) / len(y_true)
+        return round(acc, 2)
 
 
 
