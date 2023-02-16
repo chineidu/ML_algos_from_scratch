7ede23f4700620113874079747de75ab34464c64
Chinedu Ezeofor <Chineiduezeofor@gmail.com>
2023-02-16 21:37:50 +0100

    chore: Update linear regression algorithm

--- src/linear_regression.py
+++ src/linear_regression.py
@@ -1,9 +1,11 @@
-"""This module is used to calculate Liinear Regression from scratch."""
+"""This module is used to build Linear Regression from scratch."""
 
 import numpy as np
 
+from src import Model
 
-class LinearRegression:
+
+class LinearRegression(Model):
     """This is an implementation of linear regression."""
 
     def __init__(self, learning_rate: float = 0.001, n_iters: int = 1_000) -> None:
@@ -13,43 +15,45 @@ class LinearRegression:
         self.bias = None
 
     def __repr__(self) -> str:
-        return f"{__class__.__name__}(learning_rate={self.learning_rate}, n_iters={self.n_iters:,})"
+        return (
+            f"{__class__.__name__}(learning_rate={self.learning_rate!r}, "
+            f"n_iters={self.n_iters:,})"
+        )
 
     def fit(self, X=np.ndarray, y=np.ndarray) -> None:
-        """This is used to train the linear regression model."""
         n_samples, n_features = X.shape
 
-        # Step1: Initialize the weight and bias
+        # Step 1: Initialize the weight and bias
         self.weight = np.zeros((n_features))  # Vector
         self.bias = 0  # Scalar
 
-        # Step2: Estimate the y_value given the data points
+        # Step 2: Estimate the y_value given the data points
         # Note: shape of X: (n_samples, n_features) and shape of weight: (n_features, 1)
         # Dot product: (A, B) x (B, C). i.e the inner dimensions MUST be equal.
         # For more info check: https://numpy.org/doc/stable/reference/generated/numpy.dot.html
         for _ in range(self.n_iters):
             y_pred = np.dot(X, self.weight) + self.bias
 
-            # Step3: Calculate the change in weight and bias values for each training
+            # Step 3: Calculate the change in weight and bias values for each training
             # example using gradient descent.
             # shape of x_i: (1, n_features), shape of (y - y_hat): (1,) a rank1 array
-            # so we need to transpose x_i. Note that np.dot also performs the summation.
+            # so we need to transpose x_i. so that shape of x_i.T: (n_features, 1)
+            # Note that np.dot also performs the summation.
             dw = (1 / n_samples) * 2 * (np.dot(X.T, (y_pred - y)))
-            db = (1 / n_samples) * 2 * np.sum(y_pred - y)
+            db = 2 * np.mean(y_pred - y)
 
-            # Step4: Update the parameters
+            # Step 4: Update the parameters
             self.weight -= self.learning_rate * dw
             self.bias -= self.learning_rate * db
         return self
 
-    def predict(self, X: np.ndarray) -> float:
-        """This is used to make predictions."""
-        # Step5. Use the update parameters to make predictions.
+    def predict(self, X: np.ndarray) -> np.ndarray:
+        # Step 5. Use the updated parameters to make predictions.
         y_pred = np.dot(X, self.weight) + self.bias
         return y_pred
 
     @staticmethod
-    def calculate_MSE(y_true: np.ndarray, y_pred: np.ndarray) -> float:
+    def calculate_mean_squared_error(y_true: np.ndarray, y_pred: np.ndarray) -> float:
         """This is used to calculate the mean square error."""
         mse = np.mean(np.square(y_true - y_pred))
         return round(mse, 2)
