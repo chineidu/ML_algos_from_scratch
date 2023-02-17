51bdb70fb710820b2c81aac909e2f1aa66f8ea72
Chinedu Ezeofor <Chineiduezeofor@gmail.com>
2023-02-17 18:24:37 +0100

    chore: Test Naive Bayes algorithm

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 
 plt.style.use("ggplot")
 
-from sklearn.datasets import make_regression
+from sklearn.datasets import make_regression, make_classification
 from sklearn.model_selection import train_test_split
 
 from src.linear_regression import LinearRegression
@@ -17,24 +17,38 @@ RANDOM_STATE = 123
 TEST_SIZE = 0.1
 N_SAMPLES = 2_000
 N_FEATURES = 1
+N_CLASSES = 2
 NOISE = 10
 
 
-def generate_mock_data() -> tuple[np.ndarray, np.ndarray]:
-    """This generates the synthetic data required for regression.
+def generate_mock_data(*, type_: str) -> tuple[np.ndarray, np.ndarray]:
+    """This generates the synthetic data required for classification
+    or regression.
 
     Params:
-        None
+        type_ (str): 'classification' or 'regression'
 
     Returns:
         (X, y) (tuple): It returns the predictor and the target variable.
     """
-    data = make_regression(
+    type_value = ["classification", "regression"]
+    if type_ not in type_value:
+        raise ValueError(f"{type_!r} should be {type_value[0]!r} or {type_value[1]!r}.")
+
+    regres_data = make_regression(
         n_samples=N_SAMPLES,
         n_features=N_FEATURES,
         noise=NOISE,
         random_state=RANDOM_STATE,
     )
+    classif_data = make_classification(
+        n_samples=N_SAMPLES,
+        n_features=N_FEATURES + 10,
+        n_classes=N_CLASSES,
+        random_state=RANDOM_STATE,
+    )
+
+    data = regres_data if type == "regression" else classif_data
     X, y = data
     return (X, y)
 
@@ -57,7 +71,7 @@ def visualize(*, X: np.ndarray, y: np.ndarray) -> Plot:
 def run(learning_rate: float, n_iters: int, verbose: int):
     """Train and evaluate the model."""
     # create a synthetic data
-    X, y = generate_mock_data()
+    X, y = generate_mock_data(type_="regression")
 
     # split the data
     X_train, X_test, y_train, y_test = train_test_split(
@@ -90,4 +104,14 @@ def run(learning_rate: float, n_iters: int, verbose: int):
 
 
 if __name__ == "__main__":
-    mse = run()
+    # mse = run()
+    from src.naive_bayes import NaiveBayes
+
+    nb = NaiveBayes()
+    X, y = generate_mock_data(type_="classification")
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=TEST_SIZE, random_state=RANDOM_STATE
+    )
+    nb.fit(X_train, y_train)
+    y_pred = nb.predict(X_test)
+    print(np.mean(y_pred == y_test))
