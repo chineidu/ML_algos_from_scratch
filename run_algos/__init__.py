1d933767cfe948800ac9274b0dfa28b7d14413e0
Chinedu Ezeofor <Chineiduezeofor@gmail.com>
2023-02-18 11:07:17 +0100

    chore: Init variables

---
+++ run_algos/__init__.py
@@ -0,0 +1,4 @@
+# pylint: disable=missing-module-docstring
+from run_algos.utils import RANDOM_STATE as RANDOM_STATE
+from run_algos.utils import TEST_SIZE as TEST_SIZE
+from run_algos.utils import generate_mock_data as generate_mock_data
